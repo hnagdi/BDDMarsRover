d54e87206a56d9adacea21d7a7e03c7dffbaf60e
Hojefa Nagdi <hojefa.nagdi@proxibid.com>
2018-07-18 10:16:57 +0530

    improvements

--- src/main/java/com/mars/rover/exception/MoveNotPossibleException.java
+++ src/main/java/com/mars/rover/exception/MoveNotPossibleException.java
@@ -1,7 +1,7 @@
 package com.mars.rover.exception;
 
-import com.mars.rover.enums.DirectionEnum;
 import com.mars.rover.Position;
+import com.mars.rover.enums.DirectionEnum;
 
 public class MoveNotPossibleException extends CommandExecutionException {
 
